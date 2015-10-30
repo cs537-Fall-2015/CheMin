0b13407158c0ed2bdc5908801a140677c9d52d1f
rajnish6191 <onlinerajnish@yahoo.com>
2015-10-29 21:17:28 -0700

    Updated

---
+++ Threads with JSON/CS537_CheMin/src/CCU/CCU_Client.java
@@ -0,0 +1,38 @@
+package CCU;
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import org.json.simple.JSONObject;
+
+import json.Constants;
+import json.GlobalReader;
+
+import generic.RoverClientRunnable;
+
+public class CCU_Client extends RoverClientRunnable{
+
+	public CCU_Client(int port, InetAddress host)
+			throws UnknownHostException {
+		super(port, host);
+	}
+
+	@Override
+	public void run() {
+		try{
+			System.out.println(getRoverSocket().getPort());
+			ObjectOutputStream ostr=new ObjectOutputStream(getRoverSocket().getSocket().getOutputStream());
+			GlobalReader gr=new GlobalReader(Constants.ROOT_PATH+"1");
+			JSONObject json= gr.getJSONObject();
+			ostr.writeObject(json);
+			ObjectInputStream instr=new ObjectInputStream(getRoverSocket().getSocket().getInputStream());
+			System.out.println(instr.readObject().toString());
+		}	 catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+		catch (Exception error) {
+			System.out.println("Client CCU: Error:" + error.getMessage());
+		} 	
+	}
+}
