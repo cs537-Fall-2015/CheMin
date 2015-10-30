34c9eb0184c8854b06b287c4d620982294571917
hiren5050 <hphiren96@gmail.com>
2015-10-29 18:53:22 -0700

    update in project

---
+++ Threads with JSON/CheMinGroup2/src/Power/PowerMain.java
@@ -0,0 +1,18 @@
+package Power;
+
+import java.io.IOException;
+import generic.RoverThreadHandler;
+
+public class PowerMain {
+
+	public static void main(String[] args) {
+		Power_Server ps=null;
+		try {
+			ps = new Power_Server(9013);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		Thread serverPower=RoverThreadHandler.getRoverThreadHandler().getNewThread(ps);
+		serverPower.start();
+	}
+}
