51d95729d70539a8b6e2ce16793e6d7925c31a37
VALENTIN <valentinraspi@gmail.com>
2015-11-17 14:52:58 -0800

    GUI V2

--- src/test_client_module/TestClientModuleMain.java
+++ src/test_client_module/TestClientModuleMain.java
@@ -11,6 +11,12 @@ import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JCheckBox;
 import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
 
 public class TestClientModuleMain {
 
@@ -45,19 +51,100 @@ public class TestClientModuleMain {
 	private void initialize() {
 		frmTestClient = new JFrame();
 		frmTestClient.setTitle("Test client");
-		frmTestClient.setBounds(100, 100, 450, 300);
+		frmTestClient.setBounds(100, 100, 450, 352);
 		frmTestClient.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		JComboBox comboBox = new JComboBox();
 		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Run full process", "Manual selection"}));
 		frmTestClient.getContentPane().add(comboBox, BorderLayout.NORTH);
 		
-		JCheckBox chckbxNewCheckBox = new JCheckBox("hello\r\nje");
-		chckbxNewCheckBox.setToolTipText("");
-		frmTestClient.getContentPane().add(chckbxNewCheckBox, BorderLayout.WEST);
-		
 		JButton btnSendCommandsTo = new JButton("Send commands to chemin");
 		frmTestClient.getContentPane().add(btnSendCommandsTo, BorderLayout.CENTER);
+		
+		JPanel panel = new JPanel();
+		frmTestClient.getContentPane().add(panel, BorderLayout.WEST);
+		
+		JCheckBox chckbxNewCheckBox_1 = new JCheckBox("New check box");
+		
+		JCheckBox chckbxNewCheckBox = new JCheckBox("New check box");
+		
+		JCheckBox chckbxNewCheckBox_2 = new JCheckBox("New check box");
+		
+		JCheckBox chckbxNewCheckBox_3 = new JCheckBox("New check box");
+		
+		JCheckBox chckbxNewCheckBox_4 = new JCheckBox("New check box");
+		
+		JCheckBox chckbxNewCheckBox_5 = new JCheckBox("New check box");
+		
+		JCheckBox chckbxNewCheckBox_6 = new JCheckBox("New check box");
+		
+		JCheckBox chckbxNewCheckBox_7 = new JCheckBox("New check box");
+		
+		JCheckBox chckbxNewCheckBox_8 = new JCheckBox("New check box");
+		
+		JCheckBox chckbxNewCheckBox_9 = new JCheckBox("New check box");
+		GroupLayout gl_panel = new GroupLayout(panel);
+		gl_panel.setHorizontalGroup(
+			gl_panel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel.createSequentialGroup()
+					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_panel.createSequentialGroup()
+							.addContainerGap()
+							.addComponent(chckbxNewCheckBox))
+						.addGroup(gl_panel.createSequentialGroup()
+							.addContainerGap()
+							.addComponent(chckbxNewCheckBox_2))
+						.addGroup(gl_panel.createSequentialGroup()
+							.addContainerGap()
+							.addComponent(chckbxNewCheckBox_3))
+						.addGroup(gl_panel.createSequentialGroup()
+							.addContainerGap()
+							.addComponent(chckbxNewCheckBox_4))
+						.addGroup(gl_panel.createSequentialGroup()
+							.addContainerGap()
+							.addComponent(chckbxNewCheckBox_5))
+						.addGroup(gl_panel.createSequentialGroup()
+							.addContainerGap()
+							.addComponent(chckbxNewCheckBox_6))
+						.addGroup(gl_panel.createSequentialGroup()
+							.addContainerGap()
+							.addComponent(chckbxNewCheckBox_7))
+						.addGroup(gl_panel.createSequentialGroup()
+							.addContainerGap()
+							.addComponent(chckbxNewCheckBox_8))
+						.addGroup(gl_panel.createSequentialGroup()
+							.addContainerGap()
+							.addComponent(chckbxNewCheckBox_9))
+						.addGroup(gl_panel.createSequentialGroup()
+							.addContainerGap()
+							.addComponent(chckbxNewCheckBox_1)))
+					.addContainerGap(101, Short.MAX_VALUE))
+		);
+		gl_panel.setVerticalGroup(
+			gl_panel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(chckbxNewCheckBox_1)
+					.addGap(1)
+					.addComponent(chckbxNewCheckBox)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(chckbxNewCheckBox_2)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(chckbxNewCheckBox_3)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(chckbxNewCheckBox_4)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(chckbxNewCheckBox_5)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(chckbxNewCheckBox_6)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(chckbxNewCheckBox_7)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(chckbxNewCheckBox_8)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(chckbxNewCheckBox_9)
+					.addContainerGap(32, Short.MAX_VALUE))
+		);
+		panel.setLayout(gl_panel);
 	}
-
 }
